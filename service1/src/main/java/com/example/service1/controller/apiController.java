85e28f008908cf6777800a4d55e9e3ff2aba1645
jslee <dlwltjd7778@naver.com>
2023-09-15 11:27:46 +0900

    add routing config

--- service1/src/main/java/com/example/service1/controller/apiController.java
+++ service1/src/main/java/com/example/service1/controller/apiController.java
@@ -1,9 +1,11 @@
 package com.example.service1.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/service1")
 public class apiController {
 	@GetMapping("/api1")
 	public String api1() {
